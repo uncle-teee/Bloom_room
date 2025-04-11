6916551d862abb93d6a823d230adb859927f3ef4
uncle-teee <trevorkinyanjui254@gmail.com>
2025-04-11 17:49:18 +0300

    Add AdminDashboard component and styles

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
 import GetProducts from './components/GetProducts';
 import AddProducts from './components/AddProducts';
 import SingleProduct from './components/SingleProduct';
 import AdminDashboard from './components/AdminDashboard';
-import 'boxicons';
-
+import 'boxicons'
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/SignUp" element={<SignUp />} />
-          <Route path="/SignIn" element={<SignIn />} />
-          <Route path="/" element={<GetProducts />} />
-          <Route path="/AddProducts" element={<AddProducts />} />
-          <Route path="/SingleProduct" element={<SingleProduct />} />
-          <Route path="/AdminDashboard" element={<AdminDashboard />} />
-        </Routes>
-      </div>
-    </Router>
+   <Router>
+     <div className="App">
+      <Routes>
+        <Route path="/SignUp" element= {<SignUp />} />
+        <Route path='/SignIn' element= {<SignIn />} />
+        <Route path='/' element= {<GetProducts />} />
+        <Route path='/AddProducts' element= {<AddProducts />} />
+        <Route path='/SingleProduct' element={<SingleProduct />} />
+        <Route path='/AdminDashboard' element={<AdminDashboard />} />
+
+      </Routes>
+    </div>
+   </Router>
   );
 }
 
